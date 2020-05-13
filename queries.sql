10e26c55c971c27a592467eed9d6c06281fa8ae6
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-13 15:10:54 -0700

    the next two work

--- queries.sql
+++ queries.sql
@@ -7,12 +7,24 @@ JOIN Category as c on c.Id = p.CategoryId
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-SELECT ShipVIa, Id from [Order] as o 
-Join shipper as s on s.id = o.ShipVia
-where OrderDate < "2012-08-09"
+SELECT [Order].Id , Shipper.CompanyName
+FROM [Order]
+JOIN Shipper
+ON [Order].ShipVia = Shipper.Id
+WHERE [Order].OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-
+SELECT  o.quantity, p.ProductName 
+FROM OrderDetail as o
+JOIN Product as p on p.id = o.ProductId
+WHERE o.OrderId = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.Id, c.CompanyName, e.LastName
+FROM [Order] as o
+JOIN Employee as e
+ON o.EmployeeId = e.Id
+JOIN Customer as c
+ON o.CustomerId = c.Id
